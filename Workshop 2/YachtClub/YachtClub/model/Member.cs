fba1c31471f1e6513231b06ad8ef49b52c0cbf11
Kristoffer Karlsson <kk222hk@student.lnu.se>
2015-09-10 00:58:21 +0200

    Did some model implementation

--- Workshop 2/YachtClub/YachtClub/model/Member.cs
+++ Workshop 2/YachtClub/YachtClub/model/Member.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace YachtClub.model
 {
     class Member
     {
+        private string _socialSecurityNumber;
+        private int _memberId;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int MemberId
+        {
+            get
+            {
+                return _memberId;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Model.Member.MemberId needs to be 1 or higher");
+                }
+                _memberId = value;
+            }
+        }
+        public string SocialSecurityNumber
+        {
+            get
+            {
+                return _socialSecurityNumber;
+            }
+            private set
+            {
+                if (Regex.IsMatch(value, @"^\d{2,4}-?\d{4}-?\d{4}"))
+                {
+                    throw new ArgumentException("Model.Member.SocialSecurityNubmer is not valid format");
+                }
+                _socialSecurityNumber = value;
+            }
+        }
+
+        public Member(string firstName, string lastName, string socialSecurityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
     }
 }

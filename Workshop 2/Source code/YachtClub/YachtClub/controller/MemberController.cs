fb1acd8c379288ff892fbf3069189e26ea2f6f13
Kristoffer Karlsson <kk222hk@student.lnu.se>
2015-09-15 14:39:22 +0200

    Refactor

--- Workshop 2/Source code/YachtClub/YachtClub/controller/MemberController.cs
+++ Workshop 2/Source code/YachtClub/YachtClub/controller/MemberController.cs
@@ -8,78 +8,41 @@ namespace YachtClub.controller
 {
     class MemberController
     {
-        private model.MemberList m_memberList;
-        private view.NavigationView m_navigationView;
-        private view.MemberListView m_memberListView;
-        private view.BaseInputView m_inputView;
+        private model.MemberList m_list;
+        private view.NavigationView m_nav;
+        private view.MemberListView m_listView;
+        private view.BaseInputView m_input;
         private view.MemberView m_memberView;
         private view.BoatView m_boatView;
 
         public MemberController()
         {
-            m_memberList = new model.MemberList();
-            m_navigationView = new view.NavigationView();
-            m_inputView = new view.BaseInputView();
+            m_list = new model.MemberList();
+            m_nav = new view.NavigationView();
+            m_input = new view.BaseInputView();
             m_boatView = new view.BoatView(); ;
-            m_memberListView = new view.MemberListView(m_memberList);
+            m_listView = new view.MemberListView(m_list);
         }
 
-        public void DoHandleMember(model.Member member)
+        private void DoHandleMember(model.Member member)
         {
             m_memberView = new view.MemberView(member);
             m_memberView.ShowMember();
             view.MemberView.MemberHandleOperation whatDo = m_memberView.GetWhatToDoWithUser();
+
             switch (whatDo)
             {
                 case YachtClub.view.MemberView.MemberHandleOperation.Edit:
-                    string name = m_inputView.GetStringFromUser("Input name: ");
-                    string socialSecurityNumber = m_inputView.GetStringFromUser("Input social security number: ");
-                    member.Name = name;
-                    member.SocialSecurityNumber = socialSecurityNumber;
-                    m_memberView.ShowEditConfirmMessage(member);
+                    DoEditMember(member);
                     break;
                 case YachtClub.view.MemberView.MemberHandleOperation.Delete:
-                    try
-                    {
-                        if (m_memberView.ConfirmDelete())
-                        {
-                            m_memberList.DeleteMember(member);
-                            m_memberView.ShowSuccessDeleteMessage();
-                        }
-                        else
-                        {
-                            m_memberView.ShowFailedDeleteMessage();
-                        }
-                    }
-                    catch
-                    {
-                        m_memberView.ShowFailedDeleteMessage();
-                    }
+                    DoDeleteMember(member);
                     break;
                 case YachtClub.view.MemberView.MemberHandleOperation.RegisterBoat:
-                    m_boatView.ShowRegistrationStart();
-                    m_boatView.DisplayBoatTypes();
-                    model.BoatCategory category = m_boatView.GetCategoryInput();
-                    double length = m_inputView.GetDoubleFromUser("Input boat length: ");
-                    int id = m_inputView.GetIntegerFromUser("Input boat ID: ");
-                    member.RegisterBoat(new model.Boat(length, category, id));
+                    DoRegisterBoat(member);
                     break;
                 case YachtClub.view.MemberView.MemberHandleOperation.DeleteBoat:
-                    m_memberView.ShowMembersBoats();
-                    while (true)
-                    {
-                        try
-                        {
-                            model.Boat boatToDelete = m_memberView.GetBoatToDelete();
-                            member.DeleteBoat(boatToDelete);
-                            m_memberView.ShowDeleteBoatSuccessMessage(boatToDelete.BoatId);
-                            break;
-                        }
-                        catch
-                        {
-                            m_memberView.ShowDeleteBoatFailureMessage();
-                        }   
-                    }
+                    DoDeleteBoat(member);
                     break;
                 case YachtClub.view.MemberView.MemberHandleOperation.Back:
                     break;
@@ -88,61 +51,108 @@ namespace YachtClub.controller
             }
         }
 
-        public void DoAddMember()
+        private void DoDeleteMember(model.Member memberToDelete)
         {
             try
             {
-                string name = m_inputView.GetStringFromUser("Input name: ");
-                string socialSecurityNumber = m_inputView.GetStringFromUser("Input social security number: ");
-                int memberId = m_inputView.GetIntegerFromUser("Input user ID: ");
-                model.Member member = new model.Member(name, socialSecurityNumber, memberId);
-                m_memberList.RegisterMember(member);
-                m_inputView.PrintAddSuccess(name);
+                if (m_memberView.ConfirmDelete())
+                {
+                    m_list.DeleteMember(memberToDelete);
+                    m_memberView.ShowSuccessDeleteMessage();
+                }
+                else
+                {
+                    m_memberView.ShowFailedDeleteMessage();
+                }
             }
             catch
             {
-                Console.WriteLine("Hello");
-                m_inputView.PrintAddFailure();
+                m_memberView.ShowFailedDeleteMessage();
+            }
+        }
+
+        private void DoDeleteBoat(model.Member memberWhoseBoatToDelete)
+        {
+            m_memberView.ShowMembersBoats();
+            while (true)
+            {
+                try
+                {
+                    model.Boat boatToDelete = m_memberView.GetBoatToDelete();
+                    memberWhoseBoatToDelete.DeleteBoat(boatToDelete);
+                    m_memberView.ShowDeleteBoatSuccessMessage(boatToDelete.BoatId);
+                    break;
+                }
+                catch
+                {
+                    m_memberView.ShowDeleteBoatFailureMessage();
+                }
+            }
+        }
+
+        private void DoRegisterBoat(model.Member memberToRegisterBoat)
+        {
+            m_boatView.ShowRegistrationStart();
+            m_boatView.DisplayBoatTypes();
+            model.BoatCategory category = m_boatView.GetCategoryInput();
+            double length = m_input.GetDoubleFromUser("Input boat length: ");
+            int id = m_input.GetIntegerFromUser("Input boat ID: ");
+            memberToRegisterBoat.RegisterBoat(new model.Boat(length, category, id));
+        }
+
+        private void DoAddMember()
+        {
+            try
+            {
+                string name = m_input.GetStringFromUser("Input name: ");
+                string socialSecurityNumber = m_input.GetStringFromUser("Input social security number: ");
+                int memberId = m_input.GetIntegerFromUser("Input user ID: ");
+                model.Member member = new model.Member(name, socialSecurityNumber, memberId);
+                m_list.RegisterMember(member);
+                m_input.PrintAddSuccess(name);
+            }
+            catch
+            {
+                m_input.PrintAddFailure();
+            }
+        }
+
+        private void DoEditMember(model.Member memberToEdit)
+        {
+            string name = m_input.GetStringFromUser("Input name: ");
+            string socialSecurityNumber = m_input.GetStringFromUser("Input social security number: ");
+            memberToEdit.Name = name;
+            memberToEdit.SocialSecurityNumber = socialSecurityNumber;
+            m_memberView.ShowEditConfirmMessage(memberToEdit);
+        }
+
+        private void DoListMembers(bool compressedList)
+        {
+            try
+            {
+                model.Member memberChosenFromList = m_listView.GetUserChosenFromList(compressedList);
+                DoHandleMember(memberChosenFromList);
+            }
+            catch
+            {
+                DoRun();
             }
         }
 
         public void DoRun()
         {
-            m_navigationView.ClearMenu();
-            m_navigationView.ShowStartMenu();
-            view.NavigationView.Choices option = m_navigationView.GetStartMenuOption();
+            m_nav.ClearMenu();
+            m_nav.ShowStartMenu();
+            view.NavigationView.Choices option = m_nav.GetStartMenuOption();
+
             switch (option)
             {
                 case view.NavigationView.Choices.ListUsersCompact:
-                    m_memberListView.ShowMembers(true);
-                    model.Member memberCompact = null;
-                    while (true)
-                    {
-                        int id = m_inputView.GetIntegerFromUser("Select user by ID: ");
-                        memberCompact = m_memberList.GetMemberById(id);
-                        if (memberCompact != null)
-                        {
-                            break;
-                        }
-                        Console.WriteLine("No user with id {0}", id);
-                    }
-                    DoHandleMember(memberCompact);
+                    DoListMembers(true);
                     DoRun();
                     break;
                 case view.NavigationView.Choices.ListUsersVerbose:
-                    m_memberListView.ShowMembers(false);
-                    model.Member memberVerbose = null;
-                    while (true)
-                    {
-                        int id = m_inputView.GetIntegerFromUser("Select user by ID: ");
-                        memberVerbose = m_memberList.GetMemberById(id);
-                        if (memberVerbose != null)
-                        {
-                            break;
-                        }
-                        Console.WriteLine("No user with id {0}", id);
-                    }
-                    DoHandleMember(memberVerbose);
+                    DoListMembers(false);
                     DoRun();
                     break;
                 case view.NavigationView.Choices.AddMember:
--- Workshop 2/Source code/YachtClub/YachtClub/view/MemberListView.cs
+++ Workshop 2/Source code/YachtClub/YachtClub/view/MemberListView.cs
@@ -9,10 +9,44 @@ namespace YachtClub.view
     class MemberListView
     {
         private model.MemberList m_memberList;
+        private view.BaseInputView m_input;
 
         public MemberListView(model.MemberList memberList)
         {
             m_memberList = memberList;
+            m_input = new view.BaseInputView();
+        }
+        
+        /**
+         * @return model.Member if m_memberList is populated
+         * If it's not, @return null
+         */
+        public model.Member GetUserChosenFromList(bool compressList)
+        {
+            ShowMembers(compressList);
+            if (m_memberList.IsEmpty())
+            {
+                Console.WriteLine("-            No users are registered           -");
+                Console.WriteLine("-            Press any key to return           -");
+                Console.WriteLine("------------------------------------------------");
+                Console.ReadKey(true);
+                return null;
+            }
+            else
+            {
+                model.Member member = null;
+                while (true)
+                {
+                    int id = m_input.GetIntegerFromUser("Select user by ID: ");
+                    member = m_memberList.GetMemberById(id);
+                    if (member != null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("No user with id {0}", id);
+                }
+                return member;
+            }
         }
 
         public void ShowMembers(bool compressedList)
@@ -27,11 +61,6 @@ namespace YachtClub.view
                 Console.WriteLine("MemberID\t\tName\tPersonal number");
             }
             Console.WriteLine("------------------------------------------------");
-
-            if (m_memberList.IsEmpty())
-            {
-            Console.WriteLine("-            No users are registered           -");
-            }
 
             foreach (model.Member member in m_memberList.Members)
             {
@@ -49,8 +78,6 @@ namespace YachtClub.view
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("-        Press 0 to go back to main menu       -");
             Console.WriteLine("------------------------------------------------");
         }
 

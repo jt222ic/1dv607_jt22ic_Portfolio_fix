e50c27e0b60e53f1e0a6f5953c2a8973e35eacd3
Jan <jt222ic@student.lnu.se>
2015-09-28 16:48:54 +0200

    Sequence , updating building up Model, dont really know how to go with the <List>, if i should put it on controller,

---
+++ Workshop 2/YachtClub/YachtClub/Model/Member.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YachtClub.Model
+{
+    class Member
+    {
+
+        private int SecurityNumber;                                        // registrera och returnera personlighets nummer fält, måste vara privata för objektorientering
+        private int MemberId;
+        private string Name;
+
+       //  List<MemberList> MemberList = new List<MemberList>();     tror jag ska skapa listan från en annan klass         kanske vid controller//   skapa en lista av Member klassen ?? är det interface eller bara en lista av en klass
+        // List<Boat> list = new List<Boat>();                                // skapar en lista av båt klassen senare
+
+        
+
+
+        public int GetSecurityNumber
+        {
+            get {
+                return SecurityNumber;
+            }
+
+            set
+            {
+                SecurityNumber = value;                       
+            }
+
+        }
+
+
+        public string GetName
+        {
+
+            get{
+                return Name;
+            }
+            set
+            {
+
+                if(Name.Length <=0)
+                {
+
+                    throw new ArgumentException("Characters need at least more than 1 letter");
+                }
+
+                Name = value;
+            }
+
+        }
+
+        public int GetMemberID
+        {
+            get
+            {
+                return MemberId;
+            }
+            set
+            {
+                if(MemberId== null)
+                {
+                    throw  new ArgumentException("Boat Id doesnt exist");
+                }
+                else if(value<= 0)
+                {
+                    throw new ArgumentException("The number require to have 1 or won't get any Id");
+                }
+                MemberId = value;
+            }
+        }
+
+       
+
+
+
+        public Member(string name, int memberId, int securityname)
+        {
+            Name = name;
+            MemberId = memberId;
+            SecurityNumber = securityname;
+        }
+
+
+    }
+}
